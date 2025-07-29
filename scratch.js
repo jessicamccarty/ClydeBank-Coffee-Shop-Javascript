6c5ccde4f1e114bfdc3559da4ab79702e4cdbd1a
jessicamccarty <jessicaeitr@gmail.com>
2025-07-28 23:56:46 -0500

    Completed through Chapter 2.

---
+++ scratch.js
@@ -0,0 +1,10 @@
+document.getElementById("content").textContent = "Hello, World!"
+
+// This is how you start a single line comment
+/* This is how you make a multi-line comment */
+
+// You only need to use semi-colons when yo put two statements on the same line.
+// EX: console.log("Hello,World!"); console.log("And Hello Again!");
+
+
+
